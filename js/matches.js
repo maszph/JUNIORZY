d96f893686b51592e9bf4156326dbb3a473e6b4f
maszph <pawelekhanzel@gmail.com>
2026-08-17 23:34:47 +0200

    412341241234123

--- js/matches.js
+++ js/matches.js
@@ -10,110 +10,123 @@ const matches = [
         type: "DOM"
     },
     {
-        id: 3,
+        id: 2,
         opponent: "KS 1.FC KATOWICE (SPARING)",
         date: "2026-07-27",
         time: "20:00",
-        place: "Boiska Sportowe RAPIDego 6, Katowice",
+        place: "Boiska Sportowe RAPID",
+        address: "Jana Nepomucena Stęślickiego 6, 40-120 Katowice",
+        type: "WYJAZD"
+    },
+    {
+        id: 3,
+        opponent: "KS BURZA BOROWA WIEŚ",
+        date: "2026-08-30",
+        time: "10:00",
+        place: "Boisko piłkarskie KS Burza Borowa Wieś",
+        address: "Piaskowa 57, 43-190 Mikołów",
         type: "WYJAZD"
     },
     {
         id: 4,
-        opponent: "KS BURZA BOROWA WIEŚ",
-        date: "2026-08-30",
-        time: "10:00",
-        place: "",
-        type: "WYJAZD"
-    },
-    {
-        id: 5,
         opponent: "UKS UNIA II STRZYBNICA",
         date: "2026-09-06",
         time: "10:00",
-        place: "",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
         type: "DOM"
     },
     {
-        id: 6,
+        id: 5,
         opponent: "GKS GWAREK ORNONTOWICE",
         date: "2026-09-09",
         time: "17:00",
-        place: "",
+        place: "Górniczy Klub Sportowy Gwarek Ornontowice",
+        address: "Zwycięstwa 7B, 43-178 Ornontowice",
         type: "WYJAZD"
     },
     {
-        id: 7,
+        id: 6,
         opponent: "GKS ANDALUZJA BRZOZOWICE - KAMIEŃ",
         date: "2026-09-13",
         time: "10:00",
-        place: "",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
+        type: "DOM"
+    },
+    {
+        id: 7,
+        opponent: "UKS SOKÓŁ ZBROSŁAWICE",
+        date: "2026-09-20",
+        time: "10:00",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
         type: "DOM"
     },
     {
         id: 8,
-        opponent: "UKS SOKÓŁ ZBROSŁAWICE",
-        date: "2026-09-20",
-        time: "10:00",
-        place: "",
-        type: "DOM"
-    },
-    {
-        id: 9,
         opponent: "GKS SZOMBIERKI BYTOM",
         date: "2026-09-27",
         time: "10:00",
-        place: "",
+        place: "Boisko ze sztuczną nawierzchnią GKS Szombierki Bytom",
+        address: "8WJ2+G3, 41-907 Bytom",
         type: "WYJAZD"
     },
     {
-        id: 10,
+        id: 9,
         opponent: "MKS ZABORZE",
         date: "2026-10-04",
         time: "10:00",
-        place: "",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
         type: "DOM"
     },
     {
-        id: 11,
+        id: 10,
         opponent: "TS TARNOWICZANKA",
         date: "2026-10-11",
         time: "10:00",
-        place: "",
+        place: "Towarzystwo Sportowe Tarnowiczanka",
+        address: "Śniadeckiego, 42-600 Tarnowskie Góry",
         type: "WYJAZD"
     },
     {
-        id: 12,
+        id: 11,
         opponent: "TS ASY SPARTA ZABRZE",
         date: "2026-10-18",
         time: "10:00",
-        place: "",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
         type: "DOM"
     },
     {
-        id: 13,
+        id: 12,
         opponent: "KS MOSiR STAL ZABRZE",
         date: "2026-10-25",
         time: "10:00",
-        place: "",
+        place: "Stadion Miejski Ośrodek Sportu i Rekreacji w Zabrzu Sp. z o.o.",
+        address: "Bytomska 2b, 41-800 Zabrze",
         type: "WYJAZD"
     },
     {
-        id: 14,
+        id: 13,
         opponent: "KS BRASIL",
         date: "2026-11-08",
         time: "10:00",
-        place: "",
+        place: "Stadion Czarni Kozłowa Góra",
+        address: "ul. Pokoju, Kozłowa Góra",
         type: "DOM"
     },
     {
-        id: 15,
+        id: 14,
         opponent: "FUNDACJA MIŁOŚNIKÓW SPORTU KOLEJARZ",
         date: "2026-11-15",
         time: "10:00",
-        place: "",
+        place: "KKS Kolejarz Gliwice",
+        address: "Sportowa 21, 44-100 Gliwice",
         type: "WYJAZD"
-    }
-    /*
+    },
+        /*
     {
         id: 3,
         opponent: "TS GWAREK TARNOWSKIE GÓRY",
